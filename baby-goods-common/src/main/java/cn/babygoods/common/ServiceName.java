eae92bbeb3c42342ed0984242b8ea1bdcc64ca6f
Vincent <bayernforever@163.com>
2017-04-05 17:23:13 +0800

    getGoodsType

---
+++ baby-goods-common/src/main/java/cn/babygoods/common/ServiceName.java
@@ -0,0 +1,9 @@
+package cn.babygoods.common;
+
+/**
+ * Created by Vincent on 2017/4/5.
+ */
+public class ServiceName {
+    public static final String GetGoodsType = "getGoodsType";
+    public static final String SaveGoodsType = "saveGoodsType";
+}
--- baby-goods-service/src/main/java/cn/babygoods/service/controller/ServiceController.java
+++ baby-goods-service/src/main/java/cn/babygoods/service/controller/ServiceController.java
@@ -1,15 +1,22 @@
 package cn.babygoods.service.controller;
 
 import cn.babygoods.biz.GoodsTypeBiz;
+import cn.babygoods.biz.bo.GoodsTypeBo;
+import cn.babygoods.common.BeanUtil;
+import cn.babygoods.common.JsonUtil;
+import cn.babygoods.common.ServiceName;
 import cn.babygoods.service.vo.GetDataRequest;
 import cn.babygoods.service.vo.GetDataResponse;
+import cn.babygoods.service.vo.GoodsTypeVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Vincent on 2017/3/22.
@@ -24,22 +31,27 @@ public class ServiceController {
         GetDataResponse response = new GetDataResponse();
         try {
             String functionName = request.getFunctionName();
-            switch (functionName)
-            {
-                case "getGoodsType":
+            switch (functionName) {
+                case ServiceName.GetGoodsType:
+                    response.setData(GetGoodsType());
                     break;
-                case "saveGoodsType":
+                case ServiceName.SaveGoodsType:
                     break;
             }
 
             response.setResult("success");
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             response.setResult("fail");
             response.setMsg(ex.toString());
         }
         response.setTimeStamp(new Timestamp(new Date().getTime()).toString());
         return response;
+    }
+
+    private String GetGoodsType() throws SQLException {
+        List<GoodsTypeBo> goodsTypeBoList = goodsTypeBiz.getGoodsType();
+        List<GoodsTypeVo> goodsTypeVoList = BeanUtil.copyProperties(goodsTypeBoList, GoodsTypeVo.class);
+        String result = JsonUtil.beanToJson(goodsTypeVoList);
+        return result;
     }
 }
